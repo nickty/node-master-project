a61a8baabdefa15ece00d83b79348db5c229dfe6
Mizanur Rahman <mizanur.rahman@navana.com>
2022-02-23 07:25:59 +0600

    getting path from user and trimmed out it

--- index.js
+++ index.js
@@ -5,10 +5,23 @@
 // Dependency
 const http = require('http');
 
+const url = require('url');
+
 // the server should response all the request with a string
 
 const server = http.createServer((req, res) => {
+  //get the url and parse it
+  const parsedUrl = url.parse(req.url, true);
+
+  // get the path from url, untrimmed path
+  const path = parsedUrl.pathname;
+  const trimmedPath = path.replace(/^\/+"\/+$/g, '');
+
+  // send the response
   res.end('Bismillah');
+
+  // log the request path
+  console.log('path is: ', trimmedPath);
 });
 
 // start the server with a port
