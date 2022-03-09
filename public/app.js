6cde57632addd483438b81f6da4c8dd730f5345c
Mizanur Rahman <mizanur.rahman@navana.com>
2022-03-09 06:35:02 +0600

    just before cli tool

--- public/app.js
+++ public/app.js
@@ -4,4 +4,22 @@
 
 var app = {};
 
-console.log('console');
+app.config = {
+  sessionToken: false,
+};
+
+// Ajax client for the resful api
+app.client = {};
+
+// Interface for making api calls
+app.client.request = (
+  headers,
+  path,
+  method,
+  queryStringObject,
+  payload,
+  cb
+) => {
+  // set defaults
+  headers = typeof headers == 'object' && headers !== null ? headers : {};
+};
